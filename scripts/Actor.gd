b5584a0ca7f5d1c7762e36c6ab9dce875723a5ae
Ivan Case <ivancase@berkeley.edu>
2020-11-10 06:44:19 -0800

    More updates to the Dialog System, scripted a short demo scene, and changed the cursor to a crosshair.

--- scripts/Actor.gd
+++ scripts/Actor.gd
@@ -4,20 +4,22 @@ export(Color, RGB) var textcolor
 
 var text
 var body
+var nonbody
 var voice
 var tween
 
 var speaking
 
 func _ready():
-	text = get_node("text")
 	body = get_node("body")
-	voice = get_node("voice")
+	
+	nonbody = get_node("nonbody")
+	text = nonbody.get_node("text")
+	voice = nonbody.get_node("voice")
+	
 	tween = get_node("../Tween")
 
 func act(line):
-	speaking = true
-	
 	text.bbcode_text = "[color=#{hexcode}]{line}[/color]".format({"hexcode": textcolor.to_html(), "line": line})
 	voice.play()
 	
@@ -26,5 +28,15 @@ func act(line):
 	tween.start()
 	
 	voice.stop()
+
+func enter_limelight():
+	nonbody.show()
+	if body:
+		body.modulate = Color(1, 1, 1)
+		body.playing = true
 	
-	speaking = false
+func exit_limelight():
+	nonbody.hide()
+	if body:
+		body.modulate = Color(0, 0, 0)
+		body.playing = false
--- scripts/DialogSystem.gd
+++ scripts/DialogSystem.gd
@@ -10,25 +10,48 @@ func _ready():
 	icosa = get_node("Icosahedron")
 	sphere = get_node("Sphere")
 	narrator = get_node("Narrator")
-	
-	play_dialogue([
-	[sphere, "Hello."], 
-	[icosa, "hey !!"],
-	[icosa, "can you give me an example ..."],
-	[icosa, "of a more complex dialogue ????"],
-	[sphere, "Ironic that you ask. You've just demonstrated one yourself."],
-	[icosa, ":O"]])
+
+	var key = {"i" : icosa, "s" : sphere, "n" : narrator}
+
+	play_dialogue(_parse_script(key, 
+	"n ; welcome to my very rough draft of Shot in the Dark. press space to advance text. ::" +
+	"n ; i pretty much just have a dialogue system working, and choices aren't yet in a workable state. ::" +
+	"n ; nonetheless, let's see how a scene might play out in the finished product! ::" +
+	"s ; I'd like to ask you not to shoot me. ::" +
+	"s ; You'd ruin my immaculate, spherical form. ::" +
+	"i ; i extend the emotion but less creepy !! and about my lovely, icosahedral shape :)) ::" +
+	"i ; seriously ,,, i didn't kill you !!!!!! ::" +
+	"i ; i've killed a loooooooot of mortals ... but not you ! ::" +
+	"n ; these demons don't seem very trustworthy. ::" +
+	"s ; I could not have killed you. I've been busy studying my shape for the last four millenia. ::" +
+	"s ; I would not be surprised if the jagged faux-round excuse for a prism across from me did it. ::" +
+	"i ; ... :(( ::" +
+	"n ; wow, what a crazy set of characters. truly riveting. ::" +
+	"n ; tune in after future updates to see where this wacky story goes!!!! ::" +
+	"n ; i didn't program an ending so the next time you press space i will disappear and you can consider this game over. bye bye!!"
+	))
 	
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		emit_signal("next")
+		
+func _parse_script(key, script):
+	var lines = script.strip_edges().split("::")
+	
+	var dialogue = []
+	for line in lines:
+		var lineinfo = line.strip_edges().split(";")
+		var actor = key[lineinfo[0].strip_edges()]
+		var text = lineinfo[1].strip_edges()
+		dialogue.append([actor, text])
+	return dialogue
 
 func play_dialogue(dialogue):
 	#dialogue e.g. [[sphere, "Hello."], [icosa, "hey !!"]]
 	for lineinfo in dialogue:
 		var actor = lineinfo[0]
-		var line = lineinfo[1]
-		actor.show()
-		actor.act(line)
+		var text = lineinfo[1]
+		actor.enter_limelight()
+		actor.act(text)
 		yield(self, "next")
-		actor.hide()
+		actor.exit_limelight()
